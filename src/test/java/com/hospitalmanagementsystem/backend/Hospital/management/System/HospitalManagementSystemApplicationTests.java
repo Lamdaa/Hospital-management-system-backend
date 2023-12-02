ab3efb9cfa575548b937a831ed204cf40f2741cc
Suraj Yadav <sy8070068871@gmail.com>
2023-12-02 19:25:50 +0530

    adding initial code to github

---
+++ src/test/java/com/hospitalmanagementsystem/backend/Hospital/management/System/HospitalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalmanagementsystem.backend.Hospital.management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
